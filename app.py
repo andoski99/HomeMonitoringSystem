e9e0dba626c7c7851de8a51e7d66efa8de37a2a9
andoski99 <goodsportdevelopment@icloud.com>
2023-07-05 17:43:08 +1000

    AddedVentilationFunctions

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+"""
+This is a Flask application that controls fans and retrieves temperature data from sensors.
+"""
 from flask import Flask, render_template, jsonify, request
 from Phidget22.Devices.DigitalOutput import *
 from Phidget22.PhidgetException import *
@@ -5,21 +8,177 @@ from Phidget22.Phidget import *
 from Phidget22.Net import *
 from Phidget22.Devices.TemperatureSensor import *
 from Phidget22.Devices.VoltageRatioInput import *
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+import requests
 
+# Device serial number
 DEVICE_SERIAL_NUMBER = 597183
-fan_map = {"MTFan": {'hubport': 0, 'channel': 0},
-           "GTFan": {'hubport': 0, 'channel': 1},
-           "FTFan1": {'hubport': 0, 'channel': 2},
-           "FTFan2": {'hubport': 0, 'channel': 3}}
-temperature_map = {"GT_Temp": {'hubport': 1, 'channel': 0},
-                   "OT_Temp": {'hubport': 1, 'channel': 3},
-                   "FT_Temp": {'hubport': 5, 'channel': 0},
-                   "MT_Temp": {'hubport': 4, 'channel': 0}}
+
+# Fan mapping configuration
+fan_map = {
+    "MTFan": {'hubport': 0, 'channel': 0},
+    "GTFan": {'hubport': 0, 'channel': 1},
+    "FTFan1": {'hubport': 0, 'channel': 2},
+    "FTFan2": {'hubport': 0, 'channel': 3}
+}
+
+# Temperature sensor mapping configuration
+temperature_map = {
+    "GT_Temp": {'hubport': 1, 'channel': 0},
+    "OT_Temp": {'hubport': 1, 'channel': 3},
+    "FT_Temp": {'hubport': 5, 'channel': 0},
+    "MT_Temp": {'hubport': 4, 'channel': 0}
+}
+
+# Create the Flask application
 app = Flask(__name__)
+
+# Store the initial state of the fan (False = off)
+fan_states = {
+    'GTVentilation': False,
+    'FTVentilation': False
+}
+
+# Enable Phidget server discovery
 Net.enableServerDiscovery(PhidgetServerType.PHIDGETSERVER_DEVICEREMOTE)
+
+# Add Phidget server details
 Net.addServer("ServerName", "192.168.20.193", 5661, "", 0)
 
+# Create a scheduler to run tasks in the background
+scheduler = BackgroundScheduler()
+scheduler.start()
+
+def fan_control(fan_map, fan_name, state):
+    """
+    Controls the specified fan's state.
+
+    Args:
+        fan_map (dict): A dictionary mapping fan names to their hubport and channel.
+        fan_name (str): The name of the fan to control.
+        state (bool): The desired state of the fan (True for on, False for off).
+    """
+
+    do = DigitalOutput()
+    do.setDeviceSerialNumber(DEVICE_SERIAL_NUMBER)
+    do.setIsHubPortDevice(False)
+    do.setHubPort(fan_map[fan_name]['hubport'])
+    do.setChannel(fan_map[fan_name]['channel'])
+    do.openWaitForAttachment(5000)
+    do.setState(state)
+    do.close()
+
+def check_fan_state(fan_name):
+    """
+    Check the state of the specified fan.
+    
+    Args:
+        fan_name (str): The name of the fan to check.
+        
+    Returns:
+        (bool): True if the fan is on, False otherwise.
+    """
+    do = DigitalOutput()
+    do.setDeviceSerialNumber(DEVICE_SERIAL_NUMBER)
+    do.setIsHubPortDevice(False)
+    do.setHubPort(fan_map[fan_name]['hubport'])
+    do.setChannel(fan_map[fan_name]['channel'])
+    do.openWaitForAttachment(5000)
+    state = do.getState()
+    do.close()
+    return state
+
+def activate_GT_ventilation(state):
+    """
+    Activates or deactivates the GT ventilation system by controlling the GT and MT fans.
+
+    Args:
+        state (bool): The desired state of the GT ventilation system (True for on, False for off).
+    """
+    fan_control(fan_map, 'MTFan', state)
+    fan_control(fan_map, 'GTFan', state)
+
+def activate_FT_ventilation(state):
+    """
+    Activates or deactivates the FT ventilation system by controlling the MT and FTFan2 fans.
+
+    Args:
+        state (bool): The desired state of the FT ventilation system (True for on, False for off).
+    """
+    fan_control(fan_map, 'MTFan', state)
+    fan_control(fan_map, 'FTFan1', state)
+ 
+
+
+@app.route('/api/fan/group/GTVentilation', methods=['GET', 'POST'])
+def GT_ventilation_fan_status():
+    """
+    Endpoint for getting or setting the status of the GT ventilation fans.
+
+    Returns:
+        A JSON response containing the current state of the GT ventilation fans.
+    """
+    if request.method == 'POST':
+        state = request.json.get('state', None)
+        if state is not None:
+            activate_GT_ventilation(bool(state))
+            fan_states['GTVentilation'] = bool(state)
+
+    return jsonify({'state': fan_states['GTVentilation']})
+
+@app.route('/api/fan/group/FTVentilation', methods=['GET', 'POST'])
+def FT_ventilation_fan_status():
+    """
+    Endpoint for getting or setting the status of the FT ventilation fans.
+
+    Returns:
+        A JSON response containing the current state of the FT ventilation fans.
+    """
+    if request.method == 'POST':
+        state = request.json.get('state', None)
+        if state is not None:
+            activate_FT_ventilation(bool(state))
+            fan_states['FTVentilation'] = bool(state)
+              
+    return jsonify({'state': fan_states['FTVentilation']})
+
+
+@app.route('/api/fan/<string:fanName>', methods=['GET', 'POST'])
+def fan_status(fanName):
+    """
+    Endpoint for getting or setting the status of an individual fan.
+
+    Args:
+        fanName (str): The name of the fan.
+
+    Returns:
+        A JSON response containing the current state of the fan.
+    """
+    do = DigitalOutput()
+    do.setDeviceSerialNumber(DEVICE_SERIAL_NUMBER)
+    do.setIsHubPortDevice(False)
+    do.setHubPort(fan_map[fanName]['hubport'])
+    do.setChannel(fan_map[fanName]['channel'])
+    do.openWaitForAttachment(5000)
+    if request.method == 'POST':
+        state = request.json.get('state', None)
+        if state is not None :          
+            do.setState(bool(state)).setState(bool(state))
+    state = do.getState()
+    do.close()
+    return jsonify({'state': state})
+
 def get_temperature_from_sensor(sensor_name):
+    """
+    Retrieves the temperature reading from the specified sensor.
+
+    Args:
+        sensor_name (str): The name of the temperature sensor.
+
+    Returns:
+        The temperature reading from the sensor.
+    """
     ts = TemperatureSensor()
     ts.setDeviceSerialNumber(DEVICE_SERIAL_NUMBER)
     ts.setIsHubPortDevice(False)
@@ -32,30 +191,30 @@ def get_temperature_from_sensor(sensor_name):
 
 @app.route('/api/temperature/<string:sensor_name>')
 def get_temperature(sensor_name):
+    """
+    Endpoint for retrieving the temperature reading from a sensor.
+
+    Args:
+        sensor_name (str): The name of the temperature sensor.
+
+    Returns:
+        A JSON response containing the temperature reading from the sensor.
+    """
     temperature = get_temperature_from_sensor(sensor_name)
     return jsonify({sensor_name: temperature})
 
 @app.route('/')
 def home():
+
+    """
+    Homepage endpoint.
+
+    Returns:
+        The rendered index.html template.
+    """
     return render_template('index.html')
-
-@app.route('/api/fan/<string:fanName>', methods=['GET', 'POST'])
-def fan_status(fanName):
-    do = DigitalOutput()
-    do.setDeviceSerialNumber(DEVICE_SERIAL_NUMBER)
-    do.setIsHubPortDevice(True)
-    do.setHubPort(fan_map[fanName]['hubport'])
-    do.setChannel(fan_map[fanName]['channel'])
-    do.openWaitForAttachment(5000)
-    if request.method == 'POST':
-        state = request.json.get('state', None)
-        if state is not None:
-            do.setState(bool(state))
-    state = do.getState()
-    do.close()
-    return jsonify({'state': state})
-
 
 
 if __name__ == '__main__':
+    # Run the Flask application
     app.run(debug=True, host='127.0.0.1', port=5001)
--- static/script.js
+++ static/script.js
@@ -29,13 +29,115 @@ window.onload = function () {
 
   // Update the temperatures for all sensors
   sensorNames.forEach(updateTemperature);
+
+  /// For the GT Ventilation button
+  document
+    .querySelector("#turnOnGTVentilation")
+    .addEventListener("click", function () {
+      // get the current state of the fans
+      fetch(`/api/fan/group/GTVentilation`, { method: "GET" })
+        .then((response) => {
+          //console.log(response); // Log the response
+          return response.json();
+        })
+        .then((data) => {
+          const fanState = data.state; // current state of the fan group
+
+          // toggle the state
+          const newState = !fanState;
+          const newStateText = newState ? "On" : "Off";
+
+          // set the new state
+          fetch(`/api/fan/group/GTVentilation`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ state: newState }),
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              // update the text of the button to reflect the new state
+              document.querySelector("#turnOnGTVentilationStatus").textContent =
+                newStateText;
+
+              // update the class of the circle to reflect the new state
+              const circle = document.querySelector("#GTVentilationCircle");
+              circle.classList.remove("on", "off"); // remove both classes in case one is present
+              circle.classList.add(newState ? "on" : "off"); // add the correct class
+            })
+            .catch((error) => {
+              console.error("Error:", error);
+            });
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    });
+  /// For the GT Ventilation button
+  document
+    .querySelector("#turnOnFTVentilation")
+    .addEventListener("click", function () {
+      // get the current state of the fans
+      fetch(`/api/fan/group/FTVentilation`, { method: "GET" })
+        .then((response) => {
+          //console.log(response); // Log the response
+          return response.json();
+        })
+        .then((data) => {
+          const fanState = data.state; // current state of the fan group
+
+          // toggle the state
+          const newState = !fanState;
+          const newStateText = newState ? "On" : "Off";
+
+          // set the new state
+          fetch(`/api/fan/group/FTVentilation`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ state: newState }),
+          })
+            .then((response) => response.json())
+            .then((data) => {
+              // update the text of the button to reflect the new state
+              document.querySelector("#turnOnFTVentilationStatus").textContent =
+                newStateText;
+
+              // update the class of the circle to reflect the new state
+              const circle = document.querySelector("#FTVentilationCircle");
+              circle.classList.remove("on", "off"); // remove both classes in case one is present
+              circle.classList.add(newState ? "on" : "off"); // add the correct class
+            })
+            .catch((error) => {
+              console.error("Error:", error);
+            });
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    });
 };
+
+function updateFanState(fanElementID, newState) {
+  const newStateText = newState ? "On" : "Off";
+  if (fanElementID === "turnOnGTVentilation") {
+    document.querySelector("#turnOnGTVentilationStatus").textContent =
+      newStateText;
+  } else if (fanElementID === "turnOnFTVentilation") {
+    document.querySelector("#turnOnFTVentilationStatus").textContent =
+      newStateText;
+  }
+  document.querySelector(`#${fanElementID}Circle`).style.backgroundColor =
+    newState ? "green" : "grey";
+}
 
 // Updates the temperature for a specific sensor
 function updateTemperature(sensorName) {
   // Fetch the temperature data for the sensor
   fetch(`/api/temperature/${sensorName}`)
-    .then((response) => response.json())
+    .then((response) =>  response.json())
     .then((data) => {
       const temperature = data[sensorName];
 
@@ -52,7 +154,7 @@ function updateTemperature(sensorName) {
     });
 }
 
-// Maps temperature to thermometer height
+
 // Maps temperature to thermometer height
 function mapTemperatureToHeight(temp) {
   const minTemperature = 10; // Minimum temperature on the thermometer
@@ -70,19 +172,9 @@ function mapTemperatureToHeight(temp) {
   // Convert the proportion to percentage
   const heightPercentage = proportion * 100; // This will be a value between 0 and 100
 
-  // console.log(`Temperature: ${temp}`);
-  // console.log(`Relative Temperature: ${relativeTemp}`);
-  // console.log(`Temperature Range: ${temperatureRange}`);
-  // console.log(`Proportion: ${proportion}`);
-  // console.log(`Final Height Percentage: ${heightPercentage}`);
-
   // Return the height as a percentage with '%'
   return `${heightPercentage}%`;
 }
-
-
-
-
 
 // Creates a thermometer html element based on sensor information
 /**
@@ -204,8 +296,7 @@ function createThermometer(sensorName, sensorId, sensorLabel) {
   return thermometer;
 }
 
-
 // Update temperatures at regular intervals
 setInterval(() => {
   sensorNames.forEach(updateTemperature);
-}, 5000); // Update every five seconds
+}, 60000); // Update every five seconds
