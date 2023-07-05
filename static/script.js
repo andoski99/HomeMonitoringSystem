c65e8dd553d6de54e53126463f7402f426b1913d
andoski99 <goodsportdevelopment@icloud.com>
2023-07-05 10:37:45 +1000

    Updated Temps All working

--- static/script.js
+++ static/script.js
@@ -22,23 +22,32 @@ window.onload = function () {
   sensorNames.forEach(updateTemperature);
 };
 
-// ... Rest of your JavaScript code ...
+
 
 function updateTemperature(sensorName) {
   fetch(`/api/temperature/${sensorName}`)
     .then((response) => response.json())
     .then((data) => {
       const temperature = data[sensorName];
+      // Round temperature to 1 decimal place
+      const roundedTemperature = Math.round(temperature * 10) / 10;
+
+      // Update thermometer height
       document.getElementById(sensorIdMap[sensorName]).style.height =
         mapTemperatureToHeight(temperature) + "px";
+
+document.querySelector(`#tempValue${sensorName}`).innerText = roundedTemperature + ' °C';
+
     });
-}
+  }
+
+
 
 function mapTemperatureToHeight(temp) {
   const relativeTemp = temp - 10;
   const proportion = relativeTemp / 20;
   return proportion * 100;
-}
+};
 
 // Added new code to reduce the size of the html
 function createThermometer(sensorName, sensorId, sensorLabel) {
@@ -46,7 +55,20 @@ function createThermometer(sensorName, sensorId, sensorLabel) {
   thermometer.className = "thermometer";
 
   let label = document.createElement("div");
-  label.innerText = sensorLabel;
+  label.className = "thermometer-header"; // use the class "thermometer-header" for the label
+
+  let labelSpan = document.createElement("span");
+  labelSpan.className = "label";
+  labelSpan.innerText = `${sensorLabel}: `;
+
+let tempValue = document.createElement("span");
+tempValue.id = `tempValue${sensorName}`;
+tempValue.style.whiteSpace = "nowrap";
+
+
+  label.appendChild(labelSpan);
+  label.appendChild(tempValue);
+
   thermometer.appendChild(label);
 
   let glass = document.createElement("div");
@@ -62,10 +84,6 @@ function createThermometer(sensorName, sensorId, sensorLabel) {
   glass.appendChild(redFluid);
   glass.appendChild(mercury);
   thermometer.appendChild(glass);
-
-  let tempValue = document.createElement("span");
-  tempValue.id = `tempValue${sensorName}`;
-  thermometer.appendChild(tempValue);
 
   let scale = document.createElement("div");
   scale.className = "scale";
@@ -86,21 +104,36 @@ function createThermometer(sensorName, sensorId, sensorLabel) {
   let labels = document.createElement("div");
   labels.className = "labels";
 
-  let label1 = document.createElement("div");
-  label1.className = "label";
-  label1.innerHTML = "35&deg;C";
+  // let label1 = document.createElement("div");
+  // label1.className = "label";
+  // label1.innerHTML = "35&deg;C";
 
   let label2 = document.createElement("div");
   label2.className = "label";
-  label2.innerHTML = "25&deg;C";
+  label2.innerHTML = "30&deg;C";
 
   let label3 = document.createElement("div");
   label3.className = "label";
-  label3.innerHTML = "10&deg;C";
+  label3.innerHTML = "25&deg;C";
 
-  labels.appendChild(label1);
+  let label4 = document.createElement("div");
+  label4.className = "label";
+  label4.innerHTML = "20&deg;C";
+
+  let label5 = document.createElement("div");
+  label5.className = "label";   
+  label5.innerHTML = "15&deg;C";
+  
+  let label6 = document.createElement("div");
+  label6.className = "label";
+  label6.innerHTML = "10&deg;C";
+
+  // labels.appendChild(label1);
   labels.appendChild(label2);
   labels.appendChild(label3);
+  labels.appendChild(label4);
+  labels.appendChild(label5);
+  labels.appendChild(label6);
 
   scale.appendChild(labels);
   thermometer.appendChild(scale);
@@ -108,6 +141,7 @@ function createThermometer(sensorName, sensorId, sensorLabel) {
   return thermometer;
 }
 
+
 setInterval(() => {
   sensorNames.forEach(updateTemperature);
-}, 5000); // Update every minute
+}, 5000); // Update five seconds
